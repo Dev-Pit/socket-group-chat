9dc1eef792808e1626d8c2f164928dded8ef1bdb
[samir] <[dev.and.pit@gmail.com]>
2025-04-07 21:04:10 +0545

    changed url link from client app

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:8989", {
+const socket = io("https://socket-group-chat-f710.onrender.com", {
+  //https://socket-group-chat-f710.onrender.com // http://localhost:8989
   transports: ["websocket"], // Ensure WebSocket transport is used
   reconnectionAttempts: 3, // Limit reconnections
   reconnectionDelay: 2000, // Delay between retries
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", //https://socket-group-chat.vercel.app/ Allow origins
+    origin: "https://socket-group-chat.vercel.app", //https://socket-group-chat.vercel.app/ Allow origins
     methods: ["GET", "POST"], // Specify allowed HTTP methods
   },
 });
